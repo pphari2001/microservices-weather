8e78fea029fa46270fc69b689f599da7d3e52314
Hari <pphari2001@gmail.com>
2022-11-18 15:40:46 -0800

    Add remaining for microservices

---
+++ Cloudweather.Report/DataAccess/WeatherReportDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cloudweather.Report.DataAccess
+{
+    public class WeatherReportDbContext : DbContext
+    {
+        public WeatherReportDbContext() { }
+        public WeatherReportDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<WeatherReport> WeatherReports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WeatherReport>(b => { b.ToTable("weatherreport"); });
+        }
+    }
+}
