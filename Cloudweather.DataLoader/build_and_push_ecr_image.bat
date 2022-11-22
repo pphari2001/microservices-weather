1de9d7871c481544c623212755506d69113f31ee
pphari2001 <69405386+pphari2001@users.noreply.github.com>
2022-11-22 14:24:55 -0800

    Delete build_and_push_ecr_image.bat

--- Cloudweather.DataLoader/build_and_push_ecr_image.bat
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region us-west-2 --profile weather-ecr-agent | docker login --username AWS --password-stdin 763798617373.dkr.ecr.us-west-2.amazonaws.com
-docker build -f ./Dockerfile -t cloud-weather-data-loader:latest .
-docker tag cloud-weather-data-loader:latest 763798617373.dkr.ecr.us-west-2.amazonaws.com/cloud-weather-data-loader:latest
-docker push 763798617373.dkr.ecr.us-west-2.amazonaws.com/cloud-weather-data-loader:latest
